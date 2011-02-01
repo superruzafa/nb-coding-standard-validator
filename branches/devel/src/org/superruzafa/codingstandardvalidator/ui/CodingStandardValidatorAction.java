dde5913b9a12a8116bd251fe465845ecb6a142ba
superruzafa <superruzafa@7c1b662f-a749-f197-b3bf-83aee607b783>
2011-02-01 22:05:37 +0000

    Fixes issue 1

--- branches/devel/src/org/superruzafa/codingstandardvalidator/ui/CodingStandardValidatorAction.java
+++ branches/devel/src/org/superruzafa/codingstandardvalidator/ui/CodingStandardValidatorAction.java
@@ -3,6 +3,7 @@ package org.superruzafa.codingstandardvalidator.ui;
 import java.awt.Cursor;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.io.File;
 import java.io.IOException;
 import org.openide.DialogDisplayer;
 import org.openide.NotifyDescriptor;
@@ -29,35 +30,80 @@ public final class CodingStandardValidatorAction implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent ev) {
-        CodeSniffer codeSniffer = Lookup.getDefault().lookup(CodeSnifferBuilder.class).getInstance();
-        CodingStandardViolationsTopComponent window = CodingStandardViolationsTopComponent.findInstance();
-        WindowManager.getDefault().getMainWindow().setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
-        boolean couldValidate = false;
-        String errorMessage = "";
-        try {
-            context.getLookup().lookup(EditorCookie.class).saveDocument();
-            CodingStandardValidationReport report = codeSniffer.validate(context.getPrimaryFile());
-            couldValidate = true;
-            window.setReport(report);
-            if (!window.isOpened()) {
-                window.open();
-            }
-            window.requestActive();
-        } catch (IOException ex) {
-            couldValidate = false;
-            errorMessage = ex.getMessage();
-        } catch (CodingStandardValidatorException ex) {
-            couldValidate = false;
-            errorMessage = ex.getCause().getMessage();
-        }
 
-        WindowManager.getDefault().getMainWindow().setCursor(Cursor.getDefaultCursor());
-        if (!couldValidate) {
+        String errorMessage = "";
+        CodeSniffer codeSniffer = Lookup.getDefault().lookup(CodeSnifferBuilder.class).getInstance();
+
+        if (!isCodeSnifferScriptSet(codeSniffer))
+        {
+            /**
+             * @todo I18n
+             */
+            errorMessage = "Please, set the path for the Code Sniffer script at settings page.";
+        }
+        else if (!existsCodeSnifferScript(codeSniffer))
+        {
+            /**
+             * @todo I18n
+             */
+            errorMessage = String.format("Cannot find the Code Sniffer script located at %s.", codeSniffer.getScriptPath());
+        }
+        else
+        {
+            try {
+                WindowManager.getDefault().getMainWindow().setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+
+                context.getLookup().lookup(EditorCookie.class).saveDocument();
+                CodingStandardValidationReport report = codeSniffer.validate(context.getPrimaryFile());
+
+                CodingStandardViolationsTopComponent window = CodingStandardViolationsTopComponent.findInstance();
+                window.setReport(report);
+                if (!window.isOpened()) {
+                    window.open();
+                }
+                window.requestActive();
+            } catch (IOException ex) {
+                errorMessage = ex.getMessage();
+            } catch (CodingStandardValidatorException ex) {
+                errorMessage = ex.getCause().getMessage();
+            } finally
+            {
+                WindowManager.getDefault().getMainWindow().setCursor(Cursor.getDefaultCursor());
+            }
+        }
+        if (!"".equals(errorMessage)) {
             /**
              * @todo I18n
              */
             NotifyDescriptor nd = new NotifyDescriptor.Message("Cannot validate file coding standard.\n" + errorMessage, NotifyDescriptor.ERROR_MESSAGE);
             DialogDisplayer.getDefault().notify(nd);
         }
+    }
+
+    /**
+     * Checks either the value for the Code Sniffer script path is set or not.
+     *
+     * @param codeSniffer Code Sniffer object.
+     *
+     * @return true if path is set. false otherwise.
+     */
+    private boolean isCodeSnifferScriptSet(CodeSniffer codeSniffer)
+    {
+        return !"".equals(codeSniffer.getScriptPath());
+    }
+
+    /**
+     * Checks either the path for the Code Sniffer script exists or not.
+     *
+     * @param codeSniffer Code Sniffer object.
+     *
+     * @return true if path exists. false otherwise.
+     */
+    private boolean existsCodeSnifferScript(CodeSniffer codeSniffer)
+    {
+        File cs = new File(codeSniffer.getScriptPath());
+        boolean exists = cs.exists();
+        cs = null;
+        return exists;
     }
 }
