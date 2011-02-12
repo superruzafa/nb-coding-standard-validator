664124bc91a81d582e741d024a70e0f948ec4853
superruzafa <superruzafa@7c1b662f-a749-f197-b3bf-83aee607b783>
2011-02-12 14:54:19 +0000

    Fixes issue 4

--- src/org/superruzafa/codingstandardvalidator/ui/CodingStandardValidatorAction.java
+++ src/org/superruzafa/codingstandardvalidator/ui/CodingStandardValidatorAction.java
@@ -4,14 +4,21 @@
  */
 package org.superruzafa.codingstandardvalidator.ui;
 
+import java.awt.Cursor;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import javax.swing.ProgressMonitor;
+import javax.swing.SwingWorker;
 import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
 import org.openide.loaders.DataObject;
 import org.openide.util.Exceptions;
 import org.openide.util.Lookup;
+import org.openide.windows.WindowManager;
 import org.superruzafa.codingstandardvalidator.CodingStandardValidationReport;
 import org.superruzafa.codingstandardvalidator.CodingStandardValidatorException;
 import org.superruzafa.codingstandardvalidator.codesniffer.CodeSniffer;
@@ -20,9 +27,9 @@ public final class CodingStandardValidatorAction implements ActionListener {
 
     private final DataObject context;
     private CodeSniffer codeSniffer;
-    private final static String[] validatableMimeTypesArray = {
-        "text/x-php5"
-    };
+    private final static List<String> validatableMimeTypes = Arrays.asList(new String[]{
+                "text/x-php5"
+            });
 
     public CodingStandardValidatorAction(DataObject context) {
         this.context = context;
@@ -31,40 +38,83 @@ public final class CodingStandardValidatorAction implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent ev) {
-        FileObject fileObject = context.getPrimaryFile();
-        CodingStandardViolationsTopComponent window = CodingStandardViolationsTopComponent.findInstance();
-        if (fileObject.isFolder()) {
-            window.setReport(validateDirectory(fileObject));
-        } else {
-            window.setReport(validateFile(fileObject), codeSniffer.getCodingStandardName());
-        }
+        final FileObject fileObject = context.getPrimaryFile();
+        final CodingStandardViolationsTopComponent window = CodingStandardViolationsTopComponent.findInstance();
 
-        if (!window.isOpened()) {
-            window.open();
-        }
-        window.requestActive();
-    }
+        SwingWorker worker = new SwingWorker<Void, Void>() {
 
-    protected CodingStandardValidationReport validateFile(FileObject fileObject) {
-        CodingStandardValidationReport report = null;
-        try {
-            report = codeSniffer.validate(fileObject);
-        } catch (CodingStandardValidatorException ex) {
-            report = null;
-            Exceptions.printStackTrace(ex);
-        }
-        return report;
-    }
+            private final int MillisecondsToShow = 1000;
+            private ArrayList<CodingStandardValidationReport> reports = new ArrayList<CodingStandardValidationReport>();
+            private ProgressMonitor monitor;
 
-    protected CodingStandardValidationReport[] validateDirectory(FileObject fileObject) {
-        ArrayList<CodingStandardValidationReport> reports = new ArrayList<CodingStandardValidationReport>();
-        for (FileObject subFileObject : fileObject.getChildren()) {
-            if (subFileObject.isFolder()) {
-                reports.addAll(Arrays.asList(validateDirectory(subFileObject)));
-            } else if (Arrays.asList(validatableMimeTypesArray).contains(subFileObject.getMIMEType())) {
-                reports.add(validateFile(subFileObject));
+            @Override
+            protected Void doInBackground() throws InterruptedException {
+                reports.clear();
+                /**
+                 * @todo I18n
+                 */
+                monitor = new ProgressMonitor(WindowManager.getDefault().getMainWindow(), "Validating coding standard...", "Enumerating files...", 0, 0);
+                monitor.setMillisToDecideToPopup(MillisecondsToShow);
+                ArrayList<FileObject> files = enumerateFiles(fileObject);
+                monitor.setMaximum(files.size());
+                int i = 0;
+                URI uri = FileUtil.toFile(fileObject).toURI();
+                while (!monitor.isCanceled() && i < files.size()) {
+                    FileObject file = files.get(i);
+                    monitor.setNote(uri.relativize(FileUtil.toFile(file).toURI()).getPath());
+                    try {
+                        reports.add(codeSniffer.validate(file));
+                    } catch (CodingStandardValidatorException ex) {
+                        Exceptions.printStackTrace(ex);
+                    }
+                    monitor.setProgress(i);
+                    ++i;
+                }
+
+                return null;
             }
+
+            @Override
+            protected void done() {
+                if (!monitor.isCanceled()) {
+                    if (fileObject.isFolder()) {
+                        window.setReport(reports.toArray(new CodingStandardValidationReport[reports.size()]));
+                    } else if (reports.size() > 0) {
+                        window.setReport(reports.get(0));
+                    }
+                    if (!window.isOpened()) {
+                        window.open();
+                    }
+                    window.requestActive();
+                }
+                monitor.close();
+            }
+
+            protected ArrayList<FileObject> enumerateFiles(FileObject masterFile) {
+                ArrayList<FileObject> files = new ArrayList<FileObject>();
+                if (masterFile.isFolder()) {
+                    for (FileObject file : masterFile.getChildren()) {
+                        if (file.isFolder()) {
+                            files.addAll(enumerateFiles(file));
+                        } else if (validatableMimeTypes.contains(file.getMIMEType())) {
+                            files.add(file);
+                        }
+                    }
+                } else {
+                    files.add(masterFile);
+                }
+
+                return files;
+            }
+        };
+
+        try {
+            WindowManager.getDefault().getMainWindow().setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+            worker.execute();
+        } catch (Exception ignore) {
+        } finally {
+            WindowManager.getDefault().getMainWindow().setCursor(Cursor.getDefaultCursor());
         }
-        return reports.toArray(new CodingStandardValidationReport[reports.size()]);
+
     }
 }
